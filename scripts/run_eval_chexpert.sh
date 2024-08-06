fe5f468b8143b31e5ad25c8d3d4c3d737b7fbb7e
sga23 <sga23@ic.ac.uk>
2024-08-07 00:01:20 +0100

    Updated evaluation metrics for CheXpert

--- scripts/run_eval_chexpert.sh
+++ scripts/run_eval_chexpert.sh
@@ -4,9 +4,9 @@
 PYTHON_SCRIPT="../src/evaluation/eval_chexpert.py"
 TRUE_LABELS="../data_msc_project/physionet.org/files/mimic-cxr-jpg/2.1.0/mimic-cxr-2.1.0-test-set-labeled.csv"
 PRED_LABELS_DIR="../data_msc_project/cheXpert"
-ORDERED_TEST_IDS="../data_msc_project/eval_set/ordered_test_ids.csv"
+TEST_REPORT_PATH="../data_msc_project/cheXpert/input_chexpert.csv"
 
 # Run the Python script with the specified arguments
-python $PYTHON_SCRIPT --true_labels $TRUE_LABELS --pred_labels_dir $PRED_LABELS_DIR --ordered_test_ids $ORDERED_TEST_IDS
+python $PYTHON_SCRIPT --true_labels $TRUE_LABELS --pred_labels_dir $PRED_LABELS_DIR --test_report_path $TEST_REPORT_PATH
 
 echo "F1 score computation completed."
--- src/evaluation/eval_chexpert.py
+++ src/evaluation/eval_chexpert.py
@@ -1,20 +1,132 @@
 import pandas as pd
 import os
+import re
+import copy
+import numpy as np
 import argparse
-from sklearn.metrics import f1_score
+from sklearn import metrics
 
-def process_and_compute_f1_scores(true_labels_path, pred_labels_dir, ordered_test_ids_path):
-    # Load the true labels dataframe
-    true_df = pd.read_csv(true_labels_path)
-    ordered_test_ids_df = pd.read_csv(ordered_test_ids_path)
+def evaluate_label(tar, pred, ignore_nan=False):
+    """
+    Return precision, recall, f1, and prevalence for a single label.
+    """
+    
+    if ignore_nan:
+        idx = ~(np.isnan(tar) | np.isnan(pred))
+        pred = pred[idx]
+        tar = tar[idx]
+    
+    results = {
+        'precision': np.nan,
+        'recall': np.nan,
+        'f1': np.nan,
+        'positives': int(tar.sum())
+    }
+    
+    if results['positives'] == 0:
+        # return NaN if no positive labels
+        return results
+    
+    results['precision'] = metrics.precision_score(tar, pred, zero_division=0)
+    results['recall'] = metrics.recall_score(tar, pred)
+    if results['precision'] + results['recall'] == 0:
+        results['f1'] = 0.0
+    else:
+        results['f1'] = 2 * (results['precision'] * results['recall']) / (results['precision'] + results['recall'])
 
+    
+    return results
+    
+
+def get_scores(target, prediction, categories, ignore_nan=False):
+    
+    
+    results = {}
+    for i, c in enumerate(categories):
+        results[c] = evaluate_label(target[:, i], prediction[:, i])
+    
+    # convert to dataframe
+    df = pd.DataFrame.from_dict(results, orient='index')
+    
+    return df
+
+def evaluate_labels(df_truth, df_label, method='mention'):
+    categories = list(df_truth.columns)
+    
+    # create the matrix of 0s and 1s
+    preds = copy.copy(df_label.values)
+    targets = copy.copy(df_truth.values)
+    
+    if method == 'mention':
+        # any mention is a 1
+        preds[np.isin(preds, [-1, 0, 1])] = 1
+        targets[np.isin(targets, [-1, 0, 1])] = 1
+
+        # no mention is a 0
+        preds[np.isnan(preds)] = 0
+        targets[np.isnan(targets)] = 0
+        
+        # do not ignore NaN (which we have set to 0 anyway)
+        ignore_nan=False
+    elif method == 'absence':
+        # successful prediction of absence
+        idxNonZero = preds != 0
+        idxZero = preds == 0
+        preds[idxNonZero] = 0
+        preds[idxZero] = 1
+        
+        idxNonZero = targets != 0
+        idxZero = targets == 0
+        targets[idxNonZero] = 0
+        targets[idxZero] = 1
+        
+        # ignore NaN values
+        ignore_nan=True
+    elif method == 'presence':
+        # successful prediction of presence
+        idxZero = preds != 1
+        idxNonZero = preds == 1
+        preds[idxZero] = 0
+        preds[idxNonZero] = 1
+
+        idxZero = targets != 1
+        idxNonZero = targets == 1
+        targets[idxZero] = 0
+        targets[idxNonZero] = 1
+
+        # ignore NaN values
+        ignore_nan=True
+    elif method == 'uncertain':
+        # any non-uncertain prediction is 0
+        preds[preds!= -1] = 0
+        targets[targets != -1] = 0
+        
+        # any uncertain prediction is 1
+        preds[preds == -1] = 1
+        targets[targets == -1] = 1
+        
+        # ignore NaN
+        ignore_nan=True
+    else:
+        raise ValueError(f'Unrecognized method {method}')
+        
+    df = get_scores(targets, preds, categories, ignore_nan=ignore_nan)
+    
+    return df, preds, targets
+
+def compute_chexpert_f1_scores(true_labels_path, pred_labels_dir, test_report_path):
+    
     # Define the list of classes/labels
-    labels = [
+    categories = [
         'No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Lesion', 
-        'Airspace Opacity', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 
+        'Lung Opacity', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 
         'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices'
     ]
 
+    # Test set reports
+    test_reports_df = pd.read_csv(test_report_path, header=None)
+    test_reports_df.columns = ['id', 'text']
+    
     # Initialize lists to store F1 scores for averaging later
     micro_f1_scores = []
     macro_f1_scores = []
@@ -23,31 +135,97 @@ def process_and_compute_f1_scores(true_labels_path, pred_labels_dir, ordered_tes
     # Process each prediction file in the directory
     for file_name in os.listdir(pred_labels_dir):
         if file_name.startswith("chexpert_labeled_") and file_name.endswith(".csv"):
+            print("#" * 80)
+            print(f"Processing {file_name}...")
+            print("#" * 80)
             chexpert_df = pd.read_csv(os.path.join(pred_labels_dir, file_name))
 
-            # Drop the 'Reports' column
-            chexpert_df = chexpert_df.drop(columns=['Reports'])
+            chexpert_df = chexpert_df.merge(
+                test_reports_df, how='inner', left_on='Reports', right_on='text'
+            )
 
-            # Rename the 'Lung Opacity' column to 'Airspace Opacity'
-            chexpert_df = chexpert_df.rename(columns={'Lung Opacity': 'Airspace Opacity'})
+            chexpert_df.drop_duplicates(inplace=True)
 
-            # Concatenate the ordered_test_ids with the chexpert dataframe (excluding the 'Reports' column)
-            result_df = pd.concat([ordered_test_ids_df, chexpert_df], axis=1)
+            chexpert_df['id'] = chexpert_df['id'].astype(str).str.lstrip('s')
+            chexpert_df.set_index('id', inplace=True)
+            chexpert_df.rename(columns={'Airspace Opacity': 'Lung Opacity'}, inplace=True)
+            chexpert_df = chexpert_df[categories]
 
-            # Filter true_df to only include rows with study_id present in result_df
-            filtered_true_df = true_df[true_df['study_id'].isin(result_df['study_id'])]
+            # Load the true labels dataframe
+            true_df = pd.read_csv(true_labels_path, header=0, index_col=0)
+            true_df.index.name = 'id'
+            true_df.rename(columns={'Airspace Opacity': 'Lung Opacity'}, inplace=True)
+            true_df = true_df[categories]
 
-            # Ensure both dataframes have the same study_id order
-            result_df = result_df.sort_values('study_id').reset_index(drop=True)
-            filtered_true_df = filtered_true_df.sort_values('study_id').reset_index(drop=True)
+            # Align the columns of the true_df to match the order of labels
+            true_df.sort_index(inplace=True)
+            true_df.index = true_df.index.astype(str)
+            common_ids = true_df.index.intersection(chexpert_df.index)
+            true_df = true_df.loc[common_ids]
+            chexpert_df = chexpert_df.loc[common_ids]
+
+            print('CheXpert: Mention')
+            df, preds, targets = evaluate_labels(true_df, chexpert_df, method='mention')
+
+            for c in df.columns:
+                if 'float' in str(df.dtypes[c]):
+                    df[c] = np.round(df[c], 3)
+            print(df)
+            print()
+            total_mentions = np.sum(targets)
+
+            print('CheXpert: Uncertain')
+            df, preds, targets = evaluate_labels(true_df, chexpert_df, method='uncertain')
+            df.columns = pd.MultiIndex.from_tuples([('CheXpert', c) for c in df.columns])
+            df.columns = df.columns.reorder_levels([1, 0])
+            df = df[['precision', 'recall', 'f1', 'positives']]
+
+            for c in df.columns:
+                if 'float' in str(df.dtypes[c]):
+                    df[c] = np.round(df[c], 3)
+
+            df.index.name = 'Uncertain'
+            print(df)
+            print()
 
             # Calculate micro, macro, and weighted F1 scores
-            y_true_all = filtered_true_df[labels].fillna(0).astype(int).values.flatten()
-            y_pred_all = result_df[labels].fillna(0).astype(int).values.flatten()
+            micro_f1 = metrics.f1_score(targets, preds, average='micro', zero_division=0) * (np.sum(targets) / total_mentions)
+            macro_f1 = metrics.f1_score(targets, preds, average='macro', zero_division=0) * (np.sum(targets) / total_mentions)
+            weighted_f1 = metrics.f1_score(targets, preds, average='weighted', zero_division=0) * (np.sum(targets) / total_mentions)
 
-            micro_f1 = f1_score(y_true_all, y_pred_all, labels=[-1, 0, 1], average='micro', zero_division=0)
-            macro_f1 = f1_score(y_true_all, y_pred_all, labels=[-1, 0, 1], average='macro', zero_division=0)
-            weighted_f1 = f1_score(y_true_all, y_pred_all, labels=[-1, 0, 1], average='weighted', zero_division=0)
+            print('CheXpert: Absence')
+            df, preds, targets = evaluate_labels(true_df, chexpert_df, method='absence')
+            df.columns = pd.MultiIndex.from_tuples([('CheXpert', c) for c in df.columns])
+            df.columns = df.columns.reorder_levels([1, 0])
+            df = df[['precision', 'recall', 'f1', 'positives']]
+            for c in df.columns:
+                if 'float' in str(df.dtypes[c]):
+                    df[c] = np.round(df[c], 3)
+
+            df.index.name = 'Absence'
+            print(df)
+            print()
+
+            micro_f1 += metrics.f1_score(targets, preds, average='micro', zero_division=0) * (np.sum(targets) / total_mentions)
+            macro_f1 += metrics.f1_score(targets, preds, average='macro', zero_division=0) * (np.sum(targets) / total_mentions)
+            weighted_f1 += metrics.f1_score(targets, preds, average='weighted', zero_division=0) * (np.sum(targets) / total_mentions)
+
+            print('CheXpert: Presence')
+            df, preds, targets = evaluate_labels(true_df, chexpert_df, method='presence')
+            df.columns = pd.MultiIndex.from_tuples([('CheXpert', c) for c in df.columns])
+            df.columns = df.columns.reorder_levels([1, 0])
+            df = df[['precision', 'recall', 'f1', 'positives']]
+            for c in df.columns:
+                if 'float' in str(df.dtypes[c]):
+                    df[c] = np.round(df[c], 3)
+
+            df.index.name = 'Presence'
+            print(df)
+            print()
+
+            micro_f1 += metrics.f1_score(targets, preds, average='micro', zero_division=0) * (np.sum(targets) / total_mentions)
+            macro_f1 += metrics.f1_score(targets, preds, average='macro', zero_division=0) * (np.sum(targets) / total_mentions)
+            weighted_f1 += metrics.f1_score(targets, preds, average='weighted', zero_division=0) * (np.sum(targets) / total_mentions)
 
             # Append the scores to the lists
             micro_f1_scores.append(micro_f1)
@@ -75,8 +253,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process CheXpert predictions and compute F1 scores.")
     parser.add_argument('--true_labels', type=str, required=True, help="Path to the true labels CSV file")
     parser.add_argument('--pred_labels_dir', type=str, required=True, help="Directory containing prediction CSV files")
-    parser.add_argument('--ordered_test_ids', type=str, required=True, help="Path to the ordered test IDs CSV file")
+    parser.add_argument('--test_report_path', type=str, required=True, help="Path to the test set reports CSV file")
     
     args = parser.parse_args()
 
-    process_and_compute_f1_scores(args.true_labels, args.pred_labels_dir, args.ordered_test_ids)
+    compute_chexpert_f1_scores(args.true_labels, args.pred_labels_dir, args.test_report_path)
